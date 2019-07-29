da3f263e522836ffa1385f46ed7381a39fd7397e
ginnac <ginnacampbell1990@gmail.com>
2019-07-28 20:34:25 -0600

    start game - displaying underscor es working fine

--- index.js
+++ index.js
@@ -11,27 +11,85 @@ var randomSongIndex = Math.floor(Math.random()*songs.length);
 
 //getting random song 
 var randomSong = songs[randomSongIndex];
+
+var guessesremaining = songs.length + 5; 
+
+//get the guess from user and pass it to the word object 
+
+var randomSongString = new Word(randomSong);
+
+// functionality
+
+
+    //start the game...
+    function startGame(){
+        //pass the response and run word to displayWord function it will return the underscors for the selected song: 
+        return console.log(randomSongString.displayWord());
+    }
+//function going to reset the game if remaining guesses is 0 
+
+
+    //function to check if letter is correct or not 
+
+    function verifyLetter(){
+        //check if letter is correct
+        
+
+        //console.log the word with underscores and letter that have been guessed so far
+    }
+
+    
+    //function to verify if the user won
+
+// if number of guessesremaining >0  it will keep prompting and giving chance to guess a letter of the same word
+
+if (guessesremaining > 0){
+ 
+//start with prompting
+    var questions = [
+        {
+            type: 'input',
+            name: 'word',
+            message: 'Guess a Letter!',
+            default: "",
+        },]
+
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+
+        //take response from prompting and do this: 
+        var toLog = answers.word
+
+        if(guessesremaining = songs.length + 5){
+            startGame();
+        }  
+
+            // stablish if letter is correct or not...
+
+
+
+    console.log(toLog);
+  });   
+
+
+
+    
+    
+
+
+
+
+
+
+}
+
+
+else{}
+
 // Prompts the user for each guess and keeps track of the user's remaining guesses
 
 
-var questions = [
-    {
-      type: 'input',
-      name: 'word',
-      message: 'Guess a Letter!',
-      default: "",
-    }, ]
-
-inquirer
-  .prompt(questions)
-  .then(answers => {
-    // Use user feedback for... whatever!!\754
-    var resultGuessed = new Word(randomSong).displayWord();
-  
-
-
-    console.log(resultGuessed);
-  });
 
 
 
